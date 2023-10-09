b3432c861315f24ca350a4d9670847ee99396407
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-10-08 23:41:28 -0400

    got the basic game mechanics working

--- app/components/GameBoard/GameBoard.tsx
+++ app/components/GameBoard/GameBoard.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 import './GameBoard.css'
 import { useEffect, useState } from 'react'
 import * as Tone from 'tone'
+import { getRandomInteger } from '@/app/utils/getRandomInteger'
 
 export default function GameBoard() {
   const gameStatus = useAppSelector((state) => state.gameStatusReducer)
@@ -12,9 +13,16 @@ export default function GameBoard() {
   const dispatch = useAppDispatch()
 
   // create a synth and connect it to the main output (speakers)
-  const synth = new Tone.Synth().toDestination()
+  let synth: Tone.Synth<Tone.SynthOptions>
+  if (typeof window !== 'undefined') {
+    synth = new Tone.Synth().toDestination()
+  }
 
   const [lastClickedWedge, setLastClickedWedge] = useState<string>('')
+  const [botSequence, setBotSequence] = useState<string[]>([])
+  const [playerSequence, setPlayerSequence] = useState<string[]>([])
+  const [botClick, setBotClick] = useState<string>('')
+  const [playerScore, setPlayerScore] = useState(0)
 
   const areWedgesDisabled = gameStatus.value === 'UNSTARTED'
 
@@ -30,6 +38,9 @@ export default function GameBoard() {
   const handleWedgeClick = (id: string) => {
     setLastClickedWedge(id)
     synth.triggerAttackRelease(tones[id as keyof typeof tones], '16n')
+    const newPlayerSequence = [...playerSequence]
+    newPlayerSequence.push(id)
+    setPlayerSequence(newPlayerSequence)
   }
 
   useEffect(() => {
@@ -40,7 +51,77 @@ export default function GameBoard() {
     }
   }, [lastClickedWedge, setLastClickedWedge])
 
-  if (gameStatus.value !== 'PAGELOAD') {
+  const playBotSequence = async () => {
+    for (const item of botSequence) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          setBotClick(item)
+          synth.triggerAttackRelease(tones[item as keyof typeof tones], '16n')
+          resolve(undefined)
+        }, 800)
+      })
+    }
+    setTimeout(() => {
+      setBotClick('')
+    }, 800)
+  }
+
+  useEffect(() => {
+    if (gameStatus.value === 'STARTED') {
+      if (botSequence.length === 0) {
+        setPlayerSequence([])
+        setBotSequence([String(getRandomInteger())])
+      }
+    }
+  }, [gameStatus.value, botSequence])
+
+  useEffect(() => {
+    if (gameStatus.value === 'STARTED') {
+      if (botSequence.length > playerScore) {
+        playBotSequence().catch((error) => {
+          console.error(error)
+        })
+      }
+    }
+  }, [gameStatus.value, botSequence, playerScore])
+
+  useEffect(() => {
+    if (gameStatus.value === 'STARTED') {
+      if (
+        botSequence.length > 0 &&
+        (botSequence.length === playerScore ||
+          botSequence.length === playerSequence.length)
+      ) {
+        setPlayerSequence([])
+        const newBotSequence = [...botSequence]
+        newBotSequence.push(String(getRandomInteger()))
+        setBotSequence(newBotSequence)
+      }
+    }
+  }, [gameStatus.value, botSequence, playerScore, playerSequence])
+
+  useEffect(() => {
+    if (gameStatus.value === 'STARTED') {
+      if (
+        botSequence.length > 0 &&
+        botSequence.length === playerSequence.length &&
+        botSequence.every((item, index) => playerSequence[index] === item)
+      ) {
+        setPlayerScore(playerScore + 1)
+        setPlayerSequence([])
+      }
+      if (
+        botSequence.length > 0 &&
+        playerSequence.length > 0 &&
+        !playerSequence.every((item, index) => botSequence[index] === item)
+      ) {
+        dispatch(updateGameStatus({ value: 'FINISHED' }))
+        alert(`Game over! Score: ${playerScore}`)
+      }
+    }
+  }, [gameStatus.value, botSequence, playerScore, playerSequence])
+
+  if (gameStatus.value !== 'PAGELOAD' && gameStatus.value !== 'FINISHED') {
     return (
       <div className="GameBoard">
         {wedges.map((wedge, index) => {
@@ -50,7 +131,7 @@ export default function GameBoard() {
               id={`${index}`}
               disabled={areWedgesDisabled}
               className={`GameBoard__wedge ${wedge} ${
-                lastClickedWedge === String(index)
+                lastClickedWedge === String(index) || botClick === String(index)
                   ? 'opacity-100'
                   : 'opacity-75'
               }`}
---
+++ app/utils/getRandomInteger.ts
@@ -0,0 +1,19 @@
+export const getRandomInteger = () => {
+  if (typeof window !== 'undefined' && window.crypto) {
+    const randomArray = new Uint32Array(1)
+    window.crypto.getRandomValues(randomArray)
+    return randomArray[0] % 4
+  } else if (typeof require !== 'undefined') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const crypto = require('crypto')
+    const randomBuffer = crypto.randomBytes(4)
+    const randomValue = randomBuffer.readUInt32BE(0)
+    return randomValue % 4
+  } else {
+    // Fallback for environments without crypto support
+    console.warn(
+      'Random number generation is not supported in this environment. Using Math.random() as a fallback.'
+    )
+    return Math.floor(Math.random() * 4)
+  }
+}
