995269d52d2e9cf1355ab8cac14faad809cc3317
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-10-30 21:54:53 -0400

    more troubleshooting

--- app/components/InstallationButton/InstallationButton.tsx
+++ app/components/InstallationButton/InstallationButton.tsx
@@ -13,15 +13,11 @@ const InstallationButton = () => {
   )
   const dispatch = useAppDispatch()
 
-  let deferredPrompt: IBeforeInstallPromptEvent | null = null
-
   useEffect(() => {
     const handleBeforeInstallPrompt = (e: IBeforeInstallPromptEvent) => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault()
       // Stash the event so it can be triggered later
-      console.log(e)
-      deferredPrompt = e
       dispatch(updateInstallationPrompt({ value: e }))
     }
 
@@ -37,8 +33,10 @@ const InstallationButton = () => {
     }
   }, [])
 
-  const handleInstall = () => {
-    // if (!installationPrompt.value) return
+  const handleInstall = async () => {
+    if (!installationPrompt.value) return
+    const result = await installationPrompt.value.prompt()
+    console.log(result)
     // installationPrompt.value.showPrompt()
     // installationPrompt.value.userChoice.then((choiceResult) => {
     //   if (choiceResult.outcome === 'accepted') {
@@ -47,26 +45,12 @@ const InstallationButton = () => {
     //     console.log('User dismissed the install prompt')
     //   }
     // })
-    // dispatch(updateInstallationPrompt({ value: null }))
-    if (!deferredPrompt) return
-
-    console.log(deferredPrompt)
-
-    deferredPrompt.prompt()
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt')
-      } else {
-        console.log('User dismissed the install prompt')
-      }
-    })
-
-    deferredPrompt = null // Clear the reference after using it
+    dispatch(updateInstallationPrompt({ value: null }))
   }
 
   return (
     <div>
-      {deferredPrompt && (
+      {installationPrompt.value && (
         <div className="fixed top-2 left-1/2 -translate-x-1/2 z-20 flex gap-4 items-center">
           <button onClick={handleInstall} className="Button w-[197px]">
             Install Super Simon
