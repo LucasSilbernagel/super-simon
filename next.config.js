ced2a3d7196cc5f3e3dbfc316069962d8813ec70
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-11-03 21:17:05 -0400

    testing config update

--- next.config.js
+++ next.config.js
@@ -1,22 +1,38 @@
 /** @type {import('next').NextConfig} */
 
 const prod = process.env.NODE_ENV === 'production'
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  disable: prod ? false : true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /\/_next\/static\/chunks\/pages\/.*/,
-      handler: 'CacheFirst', // Or any other strategy
-      options: {
-        cacheName: 'html-cache',
-      },
-    },
-  ],
-})
+
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  // next.js config
-})
+	reactStrictMode: true,
+	pwa: {
+		dest: "public",
+		register: true,
+    disable: prod ? false : true,
+		skipWaiting: true,
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/]
+	}
+});
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   disable: prod ? false : true,
+//   skipWaiting: true,
+//   runtimeCaching: [
+//     {
+//       urlPattern: /\/_next\/static\/chunks\/pages\/.*/,
+//       handler: 'CacheFirst', // Or any other strategy
+//       options: {
+//         cacheName: 'html-cache',
+//       },
+//     },
+//   ],
+// })
+
+// module.exports = withPWA({
+//   // next.js config
+// })
