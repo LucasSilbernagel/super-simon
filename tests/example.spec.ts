f9faebca6b0412f2f94245f289d3fcbf47c37a13
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-11-11 16:44:05 -0500

    testing action

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto(process.env.VERCEL_PREVIEW_URL || 'http://localhost:3000/')
+  // await page.goto(process.env.VERCEL_PREVIEW_URL || 'http://localhost:3000/')
   console.log(`PR preview URL: ${process.env.VERCEL_PREVIEW_URL}`)
 
-  await expect(page).toHaveTitle(/Super Simon/)
+  // await expect(page).toHaveTitle(/Super Simon/)
 })
