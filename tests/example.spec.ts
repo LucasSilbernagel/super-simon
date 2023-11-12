9770288b299da100dc4469290337139d4369abc4
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-11-11 20:31:55 -0500

    another test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
+  // TODO: use production URL as the final backup?
   await page.goto(process.env.VERCEL_PREVIEW_URL || 'http://localhost:3000/')
   console.log(`PR preview URL: ${process.env.VERCEL_PREVIEW_URL}`)
 
