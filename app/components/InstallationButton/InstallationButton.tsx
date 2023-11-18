8d72eb113b85ddc328e8afc9b858b2968e703c8f
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-11-17 23:45:01 -0500

    updated installation button animation

--- app/components/InstallationButton/InstallationButton.tsx
+++ app/components/InstallationButton/InstallationButton.tsx
@@ -46,8 +46,8 @@ const InstallationButton = () => {
   if (isButtonLoaded) {
     return (
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
         data-testid="installation-button"
       >
